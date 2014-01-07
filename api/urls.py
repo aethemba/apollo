f6cb6c0c4cf54adce1fa71115075f01fdf8236d5
Aksel <aksel.ethem@gmail.com>
2014-01-07 21:41:55 +0100

    added default api urls

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,19 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, routers
+
+class UserViewSet(viewsets.ModelViewSet):
+    model = User
+
+class GroupViewSet(viewsets.ModelViewSet):
+    model = Group
+
+router = routers.DefaultRouter()
+router.register(r"users", UserViewSet)
+router.register(r"groups", GroupViewSet)
 
 urlpatterns = patterns("",
+    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls'),
                                                 namespace="rest_framework")
 )
