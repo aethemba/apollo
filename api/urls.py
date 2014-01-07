afff49942d85de09e6e92d83ad3dfc67acd1bb09
Aksel <aksel.ethem@gmail.com>
2014-01-07 21:36:14 +0100

    added api package

---
+++ api/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns("",
+    url(r'^api-auth/', include('rest_framework.urls'),
+                                                namespace="rest_framework")
+)
