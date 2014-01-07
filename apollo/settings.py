b5bf4c3baa8a9efff3439304196f0c460499ec6d
Aksel <aksel.ethem@gmail.com>
2014-01-07 22:12:36 +0100

    added static dirs

--- apollo/settings.py
+++ apollo/settings.py
@@ -1,7 +1,9 @@
 # Django settings for apollo project.
-
+import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+PROJECT_PATH= os.path.dirname(__file__)
+BASE_PATH = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -56,6 +58,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    PROJECT_PATH + "/static"
 )
 
 # List of finder classes that know how to find static files in
