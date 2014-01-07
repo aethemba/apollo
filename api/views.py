f0675dd067c85c41f0ee8d447f0d16c75dc74bff
Aksel <aksel.ethem@gmail.com>
2014-01-07 21:54:03 +0100

    added first views in viewsets

--- api/views.py
+++ api/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from quickstart.serializers import UserSerializer, GroupSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+	"""
+	API Endpoint for users
+	"""
+
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+	"""
+	API Endpoint for GroupSerializer
+	"""
+	queryset = Group.objects.all()
+	serializer_class = GroupSerializer
+
+	
